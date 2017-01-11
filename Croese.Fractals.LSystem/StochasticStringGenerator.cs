f030e35f5c2bdcb38d6304b1e8edca2ac84591d3
Christian Roese <croese@pekininsurance.com>
2017-01-11 13:34:54 -0600

    Adds stochastic generator and its tests

---
+++ Croese.Fractals.LSystem/StochasticStringGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Croese.Fractals.LSystem
+{
+    public class StochasticStringGenerator : LindenmayerStringGenerator
+    {
+        private readonly Func<double> _randomGenerator;
+        private readonly Dictionary<char, List<Production>> _rules = new Dictionary<char, List<Production>>();
+
+        public StochasticStringGenerator(Func<double> randomGenerator)
+        {
+            _randomGenerator = randomGenerator;
+        }
+
+        protected override string Replace(char symbol)
+        {
+            List<Production> found;
+            if (!_rules.TryGetValue(symbol, out found)) return symbol.ToString();
+
+            var randomWeight = _randomGenerator() * found.Sum(p => p.Weight);
+            foreach (var production in found)
+            {
+                randomWeight -= production.Weight;
+                if (randomWeight <= 0)
+                    return production.Replacement;
+            }
+
+            return symbol.ToString();
+        }
+
+        public StochasticStringGenerator AddProduction(char symbol, double weight, string replacement)
+        {
+            List<Production> found;
+            var production = new Production
+            {
+                Weight = weight,
+                Replacement = replacement
+            };
+
+            if (_rules.TryGetValue(symbol, out found))
+                found.Add(production);
+            else
+                _rules.Add(symbol, new List<Production>
+                {
+                    production
+                });
+
+            return this;
+        }
+
+        private class Production
+        {
+            public double Weight { get; set; }
+            public string Replacement { get; set; }
+        }
+    }
+}
---
+++ Croese.Fractals.Tests/StochasticStringGeneratorTest.cs
@@ -0,0 +1,19 @@
+﻿using Croese.Fractals.LSystem;
+using Xunit;
+
+namespace Croese.Fractals.Tests
+{
+    public class StochasticStringGeneratorTest
+    {
+        [Theory]
+        [InlineData(0.2, "ABA")]
+        [InlineData(0.6, "B")]
+        public void ReturnsTheCorrectProduction(double staticRandomNumber, string expected)
+        {
+            var gen = new StochasticStringGenerator(() => staticRandomNumber);
+            gen.AddProduction('A', 0.3, "ABA");
+            gen.AddProduction('A', 0.7, "B");
+            Assert.Equal(expected, gen.Generate("A", 1));
+        }
+    }
+}
